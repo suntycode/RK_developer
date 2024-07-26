c6e5a0bb0f86981ed0abcc799e348178e1438479
RAJAT KUMAR <prajat747@gmail.com>
2024-07-27 01:31:51 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   return (
    <div>
    <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat to rock</h1>
    <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat to rock</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat to rock</h1>
    </div>
   )
 }
