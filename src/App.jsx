092c42c690004e61953b8f07bb18c785bf047cf8
RAJAT KUMAR <prajat747@gmail.com>
2024-07-27 01:10:38 +0530

    setup commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
+   
+   </div>
+  )
+}
+
+export default App
