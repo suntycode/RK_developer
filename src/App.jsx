477344c5a7f635ed1361a50b6ac67a4f93ac7b75
RAJAT KUMAR <prajat747@gmail.com>
2024-07-27 13:31:04 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
    <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
    <h1 className=' font-light text-blue-900  text-6xl'> rajat to rock</h1>
    <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat to rock</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat to rock</h1>
+   <h1 className=' font-light text-blue-900  text-6xl'> rajat reacdy</h1>
 
    <h1 className=' font-light text-blue-900  text-6xl'> rajat to rock</h1>
    </div>
